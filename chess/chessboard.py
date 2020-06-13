bfb4fa755f702a2b9f7109c869b024158f28d0be
Andrey Cholokyan <cholokyan85a@gmail.com>
2020-06-13 14:11:37 +0300

    chessboard.py: changed resolution of chessboard

--- chess/chessboard.py
+++ chess/chessboard.py
@@ -4,7 +4,7 @@
 import simple_draw as sd
 
 sd.background_color = sd.COLOR_DARK_ORANGE
-sd.resolution = (800, 800)
+sd.resolution = (840, 700)
 
 
 def cell(x, y):
@@ -12,11 +12,16 @@ def cell(x, y):
     sd.square(left_bottom=cell_point, side=side, color=color, width=filling_cell)
 
 
-side = 100
+def frame():
+    sd.square(left_bottom=sd.get_point(x=side * 2, y=side), side=side * 8, color=sd.COLOR_WHITE, width=4)
+
+
+side = 70
 filling_cell = 0
-for y in range(0, side * 8 + 1, side):
-    for x in range(0, side * 8 + 1, side):
-        color = sd.COLOR_DARK_ORANGE if (x + y) % 200 else sd.COLOR_WHITE
+for y in range(side, side * 8 + 1, side):
+    for x in range(side * 2, side * 9 + 1, side):
+        color = sd.COLOR_DARK_ORANGE if (x + y) % 140 else sd.COLOR_WHITE
         cell(x=x, y=y)
+frame()
 
 sd.pause()
